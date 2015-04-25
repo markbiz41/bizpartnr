c6d301c08eee27f6a0e2a456a9b1f8f2721a0e92
Mark <markbiz31@yahoo.com>
2015-04-24 22:14:18 -0400

    use env variables for aws

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -5,13 +5,12 @@ CarrierWave.configure do |config|
   elsif Rails.env.development?
     config.storage = :file
   else
-    config.storage = :file
-  # else
-  #   config.storage = :fog
-  #   config.fog_credentials = {
-  #     provider:              'AWS',                        # required
-  #                      # required
-  #   }
-  #   config.fog_directory  = 'bizpartnr'                          # require
+    config.storage = :fog
+    config.fog_credentials = {
+      provider:              'AWS',                        # required
+      aws_access_key_id:     ENV['AWS_ACCESS_KEY_ID'],                        # required
+      aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],                        # required
+    }
+    config.fog_directory  = ENV['S3_BUCKET_NAME']                         # require
   end
 end
